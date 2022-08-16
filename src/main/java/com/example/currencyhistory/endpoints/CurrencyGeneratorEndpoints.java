34e55b2e4dde31f0444ec8feef63bf76004db134
MadestARt <artfok@bk.ru>
2022-08-16 10:25:48 +0300

    добавил docker-compose и подправил api endpoint

--- src/main/java/com/example/currencyhistory/endpoints/CurrencyGeneratorEndpoints.java
+++ src/main/java/com/example/currencyhistory/endpoints/CurrencyGeneratorEndpoints.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum CurrencyGeneratorEndpoints {
-    CURRENCY_GENERATOR_GET("http://localhost:8081/currency-generator/v1/currency");
+    CURRENCY_GENERATOR_GET("http://currencyg:8081/currency-generator/v1/currency");
 
     private String url;
 }
